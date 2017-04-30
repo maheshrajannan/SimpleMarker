66fad39a016c6381aae7884c625c0312ee4a16c4
vigneshraja-g <vigneshraja20@gmail.com>
2017-04-30 16:13:26 +0530

    Map Click Chain Function
    1.Map Click function is addes to mapAction Listener.
    2.Marker dragend update coordinates Fixed.
    3.Top Right corner marker coordinates box added (openmap2.html) and
    corresponding css style is added to (style.css).

--- openmap coordinates/mapscript.js
+++ openmap coordinates/mapscript.js
@@ -35,33 +35,39 @@ function addMapPicker() {
               marker.getLatLng().lng+'>')
             .openPopup();
       return false;
-    }
-  }
+    },
+  
   /* Onclick update coordinates */
 
   //TODO: declare a function say mapClick and put the function here. 
 
-  function onMapClick(e) {
-    //map click event object (e) has latlng property which is a location at which the click occured.
-    $('#latInput').val(e.latlng.lat);
-    $('#lngInput').val(e.latlng.lng);
-    mapActionListener.updateMarker(e.latlng.lat, e.latlng.lng);
-  };
-  mymap.on('click', onMapClick);
+    mapClick:function(e) {
+      //map click event object (e) has latlng property which is a location at which the click occured.
+      $('#latInput').val(e.latlng.lat);
+      $('#lngInput').val(e.latlng.lng);
+      mapActionListener.updateMarker(e.latlng.lat, e.latlng.lng);
+    }
+  }
+  mymap.on('click',mapActionListener.mapClick);
 
   //TODO: declare a function say mapDragEnd and put the function here.
   //TODO: you can put these 2 functions mapDragEnd and mapClick , in to a class called mapActionListnere.
 
-  function onMapDragEnd(e) {
+  function onMarkerDragEnd(e) {
     //map click event object (e) has latlng property which is a location at which the click occured.
-    marker.bindPopup("pop drag" + '<div class="coordinput"><label for="latInput">Latitude</label><input id="latInput"/></br><label for="lngInput">Longitude</label><input id="lngInput"/></div>')
     document.getElementById('latInput').value = marker.getLatLng().lat;
     document.getElementById('lngInput').value = marker.getLatLng().lng;
-    updateMarker(e.latlng.lat, e.latlng.lng);
+    marker.bindPopup("Your location :  " +
+                marker.getLatLng().toString() + '<label for="latInput">Latitude</label>' +
+                '<input id="latInput" value=' +
+                marker.getLatLng().lat + '></br>' + '<label for="lngInput">Longitude</label>' +
+                '<input id="lngInput" value=' +
+                marker.getLatLng().lng + '>')
+            .openPopup();
   };
-  mymap.on('drag', onMapDragEnd);
+  marker.on('dragend', onMarkerDragEnd);
 
-};
+
 
 var updateMarkerByInputs = function() {
   return updateMarker($('#latInput').val(), $('#lngInput').val());
@@ -69,7 +75,7 @@ var updateMarkerByInputs = function() {
 
 $('#latInput').on('input', updateMarkerByInputs);
 $('#lngInput').on('input', updateMarkerByInputs);
-
+}
 $(document).ready(function() {
   addMapPicker();
 });
