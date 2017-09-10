50f0f346e955fa446bbb592a351d947abb325e86
vigneshraja-g <vigneshraja20@gmail.com>
2017-09-10 17:27:25 +0530

    Elastic Search REST API
    Elastic Search REST API code updated, and its working.

--- Map Book Examples/Elasticsearch_example/app.js
+++ Map Book Examples/Elasticsearch_example/app.js
@@ -1,25 +1,74 @@
 var express = require('express');
 var http = require('http');
 var elasticsearch = require('elasticsearch');
-
 var app = express();
 
 var client = new elasticsearch.Client({
   host: 'localhost:9200',
-  log: 'trace'
+  // sniffOnStart: true,
+  // sniffInterval: 60000,
 });
-client.ping({
-  // ping usually has a 3000ms timeout
-  requestTimeout: 1000
-}, function (error) {
-  if (error) {
-    console.trace('elasticsearch cluster is down!');
-  } else {
-    console.log('All is well');
+
+/* Count */
+client.count({
+  index: 'mapbook'
+}, function (error, response) {
+  console.log(response);
+});
+
+/* Creating Index */
+
+// client.index({
+//   index: 'example_index',
+//   type: 'posts',
+//   id: '1',
+//   body: {
+//     user: 'me',
+//     post_date: new Date(),
+//     message: 'Hello World!'
+//   },
+//   refresh: true
+// });
+
+/* Searching Data */
+client.search({
+  index: 'mapbook',
+  type: 'userlocation',
+  body: {
+    query: {
+      match_all: {}
+    }
   }
+}, function (error, response) {
+  console.log(response.hits);
 });
+
+// module.exports.search = function(searchData, callback) {
+//   client.search({
+//     index: 'example_index',
+//     type: 'posts',
+//     body: {
+//       query: {
+//         bool: {
+//           must: {
+//             match: {
+//               "description": searchData.searchTerm
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }).then(function (resp) {
+//     callback(resp.hits.hits);
+//   }, function (err) {
+//       callback(err.message)
+//       console.log(err.message);
+//   });
+// }
+
 /* Server */
+
 var server = http.createServer(app);
 server.listen(3000,function(){
-  console.log("Server started");
+  console.log("Server started, Port : 3000");
 });
